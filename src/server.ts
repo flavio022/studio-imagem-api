f32a13f5ded9e40b548e04f00ba54d08839c3de7
Flavio Danilo Silva da Costa <flavio@MacBook-Air-de-Flavio.local>
2022-02-04 07:42:45 -0300

    removendo module swagger

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,5 @@
 import 'reflect-metadata';
 import express from "express"
-import swaggerUi from 'swagger-ui-express';
-import swaggerFile from "./swagger.json";
 import "./database";
 import "./shared/container";
 import { router } from './routes';
@@ -12,7 +10,6 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 const port = process.env.PORT || 3331;
 app.listen(port, () => console.log("Server is running!", port));
