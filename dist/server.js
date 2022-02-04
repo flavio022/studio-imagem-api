5c29495c42b80a692f223f5a18de96d5828be210
Flavio Danilo Silva da Costa <flavio@MacBook-Air-de-Flavio.local>
2022-02-04 07:21:25 -0300

    mudando paths typescritp

---
+++ dist/server.js
@@ -0,0 +1,29 @@
+"use strict";
+
+require("reflect-metadata");
+
+var _express = _interopRequireDefault(require("express"));
+
+var _swaggerUiExpress = _interopRequireDefault(require("swagger-ui-express"));
+
+var _swagger = _interopRequireDefault(require("./swagger.json"));
+
+require("./database");
+
+require("./shared/container");
+
+var _routes = require("./routes");
+
+require("dotenv/config");
+
+var _cors = _interopRequireDefault(require("cors"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const app = (0, _express.default)();
+app.use((0, _cors.default)());
+app.use(_express.default.json());
+app.use("/api-docs", _swaggerUiExpress.default.serve, _swaggerUiExpress.default.setup(_swagger.default));
+app.use(_routes.router);
+const port = process.env.PORT || 3331;
+app.listen(port, () => console.log("Server is running!", port));
