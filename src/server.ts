5902aa303734ec41d72a9860ed2c34710d996e99
Flavio Danilo Silva da Costa <flavio@MacBook-Air-de-Flavio.local>
2022-02-05 17:09:29 -0300

    tratando erros

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,14 @@ import "./shared/container";
 import { router } from './routes';
 import 'dotenv/config';
 import cors from 'cors';
+import { handlingErrors } from "./middlewares/handlingErrors";
 
 const app = express();
 app.use(cors());
 
 app.use(express.json());
 app.use(router);
+app.use(handlingErrors);
+
 const port = process.env.PORT || 3331;
 app.listen(port, () => console.log("Server is running!", port));
