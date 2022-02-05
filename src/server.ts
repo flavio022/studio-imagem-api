fc4ff4709f4d5e9784b983a3cd5bab4923bd6908
Flavio Danilo Silva da Costa <flavio@MacBook-Air-de-Flavio.local>
2022-02-05 19:59:58 -0300

    ajustando sentry

--- src/server.ts
+++ src/server.ts
@@ -11,14 +11,7 @@ import * as Tracing from "@sentry/tracing";
 import { handlingErrors } from "./middlewares/handlingErrors";
 
 const app = express();
-Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    integrations: [
-        new Sentry.Integrations.Http({ tracing: true }),
-        new Tracing.Integrations.Express({ app }),
-    ],
-    tracesSampleRate: 1.0,
-});
+
 app.use(Sentry.Handlers.requestHandler());
 app.use(Sentry.Handlers.tracingHandler());
 
