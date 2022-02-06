bbe777ea822937bf266c8bd0de62168e888ba84f
Flavio Danilo Silva da Costa <flavio@MacBook-Air-de-Flavio.local>
2022-02-06 19:56:14 -0300

    voltando versao

--- src/server.ts
+++ src/server.ts
@@ -11,19 +11,12 @@ import * as Tracing from "@sentry/tracing";
 import { handlingErrors } from "./middlewares/handlingErrors";
 
 const app = express();
-Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    integrations: [
-        new Sentry.Integrations.Http({ tracing: true }),
-        new Tracing.Integrations.Express({ app }),
-    ],
-    tracesSampleRate: 1.0,
-});
+app.use(cors());
 app.use(Sentry.Handlers.requestHandler());
 app.use(Sentry.Handlers.tracingHandler());
 app.use(express.json());
 app.use(router);
 app.use(handlingErrors);
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 3331;
 app.listen(port, () => console.log("Server is running!", port));
