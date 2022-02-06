fc9be9e35b45ceca75df2b999413bd4744d4922d
Flavio Danilo Silva da Costa <flavio@MacBook-Air-de-Flavio.local>
2022-02-06 20:04:34 -0300

    remove error

--- src/server.ts
+++ src/server.ts
@@ -5,15 +5,15 @@ import "./shared/container";
 import { router } from './routes';
 import 'dotenv/config';
 import cors from 'cors';
+import * as Sentry from "@sentry/node";
 
 import { handlingErrors } from "./middlewares/handlingErrors";
 
 const app = express();
+app.use(express.json());
 app.use(cors());
 
-app.use(express.json());
 app.use(router);
-app.use(handlingErrors);
+app.use(Sentry.Handlers.errorHandler());
 
-const port = process.env.PORT || 3333;
-app.listen(port, () => console.log("Server is running!", port));
+app.use(handlingErrors);
