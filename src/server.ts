74d45bdc8dc67618a5e4083d2c40127a32e577cb
Flavio Danilo Silva da Costa <flavio@MacBook-Air-de-Flavio.local>
2022-03-08 12:04:46 -0300

    adicionando swagger

--- src/server.ts
+++ src/server.ts
@@ -7,13 +7,17 @@ import 'dotenv/config';
 import cors from 'cors';
 import * as Sentry from "@sentry/node";
 import { handlingErrors } from "./middlewares/handlingErrors";
+import swaggerFile from "./swagger.json";
+import swaggerUi from 'swagger-ui-express';
 
 
 const app = express();
 app.use(Sentry.Handlers.requestHandler());
 app.use(Sentry.Handlers.tracingHandler());
 app.use(express.json());
+
 app.use(cors());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 app.use(Sentry.Handlers.errorHandler());
